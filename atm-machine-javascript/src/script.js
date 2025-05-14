cd72819e02cbba733b5552ee8fd67e132be452e1
Dimma Joel <dimmajoel@gmail.com>
2025-05-14 17:57:57 +0100

    Concluded the deposit feature

--- atm-machine-javascript/src/script.js
+++ atm-machine-javascript/src/script.js
@@ -15,11 +15,29 @@ function setPin() {
   }
 }
 
+//Pin confimation feature
 function checkPinAndExecute(actionFunction) {
   const enteredPin = prompt("Enter your PIN:");
   if (enteredPin === userPin) {
     actionFunction();
   } else {
     alert("Incorrect PIN. Action Failed!");
+  }
+}
+
+//Deposit feature
+function deposit() {
+  const amount = prompt("Enter the amount to deposit:");
+  const depositAmount = parseFloat(amount);
+  if (!isNaN(depositAmount) && depositAmount > 0) {
+    balance = balance + depositAmount;
+    // transactionHistory.push(Deposit: $${numericAmount.toFixed(2)});
+    alert(
+      `Deposit of $${depositAmount.toFixed(
+        2
+      )} successful. Your new balance is $${balance.toFixed(2)}.`
+    );
+  } else {
+    alert("Invalid deposit amount.");
   }
 }
